62bc0bdf445f04ed131de3efd6d333a3490cd1b1
Junx2001 <ratsirarsonj@gmail.com>
2024-04-07 09:45:58 +0300

    [Image] Upload Image

--- src/routes/users/user.routes.js
+++ src/routes/users/user.routes.js
@@ -3,10 +3,17 @@ const checkAuth = require('../middlewares/checkAuth.middleware');
 const checkRole = require('../middlewares/checkRole.middleware');
 const userControllers = require('./user.controllers');
 const router = express.Router();
+
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage }).single('file');
+
 const UserRole = require('../../constants/UserRole');
 
-router.post('/signup', userControllers.userRegister);
+router.post('/signup', upload,  userControllers.userRegister);
 router.post('/login', userControllers.userLogin);
 router.get('/profile', checkAuth, userControllers.getProfile);
+router.post('/uploadProfile', [checkAuth,upload] , userControllers.uploadProfileImage);
+
 
 module.exports = router;
